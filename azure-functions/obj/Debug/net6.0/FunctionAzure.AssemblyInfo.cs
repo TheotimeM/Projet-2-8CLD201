77aced14f36b97170e44324b6af16bcaefaeae28
Theotime <tmamou@etu.uqac.ca>
2024-12-09 05:33:46 +0100

    modif .csproj

--- azure-functions/obj/Debug/net6.0/FunctionAzure.AssemblyInfo.cs
+++ azure-functions/obj/Debug/net6.0/FunctionAzure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FunctionAzure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c49389b8d024e8bf2a2fed8887b61e7d7bf2e595")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a69e75d25f0b380988825b18dbf36d68839674c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FunctionAzure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FunctionAzure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
